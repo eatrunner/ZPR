88eaabc0e64272e14ee076d363470f10f15a723a
Adam Kowalewski <adam@topkey.pl>
2016-12-14 16:00:42 +0100

    Temporarily removed multi-player menu position. Added 'go back' buttons. Removed unused styles

--- client/app/views/start/start.controller.js
+++ client/app/views/start/start.controller.js
@@ -8,17 +8,11 @@
 	function StartController($timeout, $scope) {
 		var $ctrl = this;
 
-		$ctrl.onItemAccept = onItemAccept;
-
 		$ctrl.menuItems = {
 			'SINGLE_PLAYER': 'single-player',
-			'MULTI_PLAYER': 'multi-player',
+			// 'MULTI_PLAYER': 'multi-player',
 			'HIGH_SCORES': 'highscores',
 			'SETTINGS': 'settings'
 		};
-
-		function onItemAccept(item) {
-			alert('You have selected: ' + item.itemText);
-		}
 	}
 })();

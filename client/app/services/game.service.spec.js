32dca234b44f13b52d5fc58ad899b809850be011
Adam Kowalewski <adam@topkey.pl>
2016-12-15 23:26:52 +0100

    updated unit test of gameService

--- client/app/services/game.service.spec.js
+++ client/app/services/game.service.spec.js
@@ -2,7 +2,46 @@
 	'use strict';
 
 	describe('Game service', function() {
+		var gameService;
+
+		beforeEach(angular.mock.module('tankGame'));
+		beforeEach(inject(function(_gameService_) {
+			gameService = _gameService_;
+		}));
+
+		it('should exist', function() {
+			expect(gameService).toBeDefined();
+		});
+
+		describe('.gameStart()', function() {
+			it('should exist', function() {
+				expect(Users.gameStart).toBeDefined();
+			});
+		});
+
+		describe('.gameStart()', function() {
+			it('should exist', function() {
+				expect(Users.gameStart).toBeDefined();
+			});
+		});
+
+		describe('.gameStart()', function() {
+			it('should exist', function() {
+				expect(Users.gameStart).toBeDefined();
+			});
+		});
+
+		describe('.gameStart()', function() {
+			it('should exist', function() {
+				expect(Users.gameStart).toBeDefined();
+			});
+		});
 		
+		describe('.gameStart()', function() {
+			it('should exist', function() {
+				expect(Users.gameStart).toBeDefined();
+			});
+		});
 	});
 
 })();

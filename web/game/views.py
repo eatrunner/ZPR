0952b630aadd02903c54bef3ef8e3d87218f2163
KArol <karol.niedzielewski@gmail.com>
2017-01-16 15:39:04 +0100

    Fixed moveplayer.

--- web/game/views.py
+++ web/game/views.py
@@ -190,7 +190,7 @@ class Controller(GameObserver):
 			}
 		else:
 			try:
-				self.game_threads_[params['game_id']].movePlayer(params['id'], params['dir'])
+				self.game_threads_[int(params['game_id'])].moveTank(int(params['id']), params['dir'])
 				return {
 				"error": ""
 				}

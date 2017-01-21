913ca245b9061d93b048885bf4ddd3a9aab998a1
macklo <maciek2608@gmail.com>
2017-01-21 17:56:58 +0100

    Tanks are now destroyed when hit by a bullet

--- game/main.py
+++ game/main.py
@@ -1,41 +1,46 @@
 from Game import Game
 from Map import Map
 from GameObserver import GameObserver
+from TestGameObserver import TestGameObserver
 from Tank import Tank
 import time
 FPS = 1
-SEC_PER_FRAME = 1.0/FPS
+SEC_PER_FRAME = 1.0 / FPS
 
-i=0
+i = 0
 
-game = Game(Map(0,13))
-gameO = GameObserver()
+game = Game(0, 13)
+gameO = TestGameObserver()
 game.addObserver(gameO)
 
-game.addTank(Tank(1, game.map.playerPos, game.map))
+game.addTank(Tank(1, [9,9], game))
+game.addTank(Tank(2, [8, 3], game))
 
 lastFrameTime = time.time()
 
 while i < 20:
-	i += 1
-	currentTime = time.time()
-	dt = currentTime - lastFrameTime
-	lastFrameTime = currentTime
-	
-	#Action example
-	if(i==3):
-		game.moveTank(0, "right")
+    i += 1
+    currentTime = time.time()
+    dt = currentTime - lastFrameTime
+    lastFrameTime = currentTime
+    print i
+    # Action example
+    if(i == 3):
+        game.moveTank(0, "right")
 
-	if(i==6):
-		game.shoot(0)
-	if(i==9):
-		game.shoot(0)
-	if i == 10:
-		game.shoot(0)
-		game.shoot(1)
+    if(i == 6):
+        game.shoot(0)
 
-	game.processGame()
+    if(i == 8):
+        game.shoot(0)
 
-	#time.sleep(currentTime + SEC_PER_FRAME - time.time())
+    if i == 10:
+        game.shoot(0)
+        game.shoot(1)
+
+    game.processGame()
+
+    #time.sleep(currentTime + SEC_PER_FRAME - time.time())
 
 game.removeTank(game.playerTank)
+game.removeTank(game.getTank(1))
